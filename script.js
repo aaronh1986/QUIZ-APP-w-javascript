6bbc63c928e15698b8b459310b799b266cb7447f
aaronh1986 <aaronharbourne@gmail.com>
2021-08-17 22:55:13 +0000

    20:25

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const startBtn = document.getElementById('start-btn');
 const nextBtn = document.getElementById('next-btn');
-const question = document.getElementById('question');
+const questionSpan = document.getElementById('question');
+let shuffledQuestions, currentQuestionIndex
+const answerContainer = document.getElementById('answer-container')
 
 startBtn.addEventListener('click', startGame);
 
@@ -8,14 +10,23 @@ function startGame() {
     console.log("I will get this and write a great quiz app");
     startBtn.classList.add('hide');
     nextBtn.classList.remove('hide');
+    showQuestion(questions);
+    currentQuestionIndex = 0;
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    setQuestions();
 }
 
 function setQuestions() {
-
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion() {
-    question.innerText = questions.question;
+function showQuestion(what) {
+        questionSpan.innerText = what.question;
+        what.answers.forEach(answer => {
+            let button = document.createElement('button');
+            button.innerText = answer.text;
+        })
+    answerContainer.appendChild(button);
 }
 
 const questions = [
